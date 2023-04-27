691523e5847e6d9d1910ac9b876dd58a820166ab
Noaman-Saleem <noaman.saleem@umt.edu.pk>
2023-04-27 12:49:48 +0500

    node export code

---
+++ 5-nodeJs/1-importCodeFromJS/index.js
@@ -0,0 +1,6 @@
+const data = require("./math.js")
+
+// console.log(data);
+// data.addition(3,5)
+// console.log(data.piValue);
+
---
+++ 5-nodeJs/1-importCodeFromJS/math.js
@@ -0,0 +1,18 @@
+// console.log(`Hello i am JS your friend!`);
+
+const PI = 3.14
+
+const add = (num1,num2)=>{
+    console.log(num1 + num2);
+}
+
+// add(3,5)
+
+// console.log(module);
+
+module.exports = {
+    addition : add,
+    piValue:PI
+}
+
+// console.log(module);
