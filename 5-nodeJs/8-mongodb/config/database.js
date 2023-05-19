c4894913b03faa3f220f695f8cfc8dba283be971
Noaman-Saleem <noaman.saleem@umt.edu.pk>
2023-05-19 12:49:40 +0500

    Add Image Quiz

--- 5-nodeJs/8-mongodb/config/database.js
+++ 5-nodeJs/8-mongodb/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDatabase = () => {
-    mongoose.connect("mongodb+srv://nomi:12345@cluster0.052clps.mongodb.net/dummy2023?retryWrites=true&w=majority").then(() => {
+    mongoose.connect("mongodb+srv://nomi:12345@cluster0.gt2mqb0.mongodb.net/dummy2023-1?retryWrites=true&w=majority").then(() => {
         console.log("Connected to MongoDB");
     }).catch((e) => {
         console.log("Oh No ERROR!");
